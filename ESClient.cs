de51b45b1ff15cf953b926e56c46d0a5b7e748bf
Geoff Irwin <geoff.irwin@Marine.ie>
2019-05-01 12:56:23 +0100

    es client and vessel class

---
+++ ESClient.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Nest;
+
+namespace wfa_es
+{
+    public class ESClient
+    {
+        private const string ES_URI = "http://10.11.1.70:9200";
+
+        private ConnectionSettings _settings;
+
+        public ElasticClient Current { get; set; }
+
+        public ESClient()
+        {
+            var node = new Uri(ES_URI);
+
+            _settings = new ConnectionSettings(node);
+            _settings.DefaultIndex("/vesselname/allnames/");
+
+            var client = new ElasticClient(_settings);
+        }
+
+    }
+}
---
+++ Vessel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wfa_es
+{
+    public class Vessel
+    {
+        public string cfr { get; set; }
+        public string CountryCode { get; set; }
+        public string VesselName { get; set; }
+        public string ExactName { get; set; }
+        public string PortCode { get; set; }
+        public string PortName { get; set; }
+        public string Loa { get; set; }
+        public string Lbp { get; set; }
+        public string TonRef { get; set; }
+        public string PowerMain { get; set; }
+    }
+}
